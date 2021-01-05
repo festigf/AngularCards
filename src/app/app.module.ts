4f7c49737216125a185c9e65b31ee677ae30e673
gf <gianfranco.festi@gmail.com>
2021-01-05 18:30:12 +0100

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { MaterialModule } from "./SharedModule/shared/material.module";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { CardComponent } from './card/card.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, CardComponent],
+  imports: [BrowserModule,FormsModule, ReactiveFormsModule, MaterialModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
