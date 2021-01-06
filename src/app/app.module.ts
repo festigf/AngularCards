55602e172710aa448a715d773f47fe4952d1a89c
gf <gianfranco.festi@gmail.com>
2021-01-06 09:45:27 +0100

    correzione directory shared

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { MaterialModule } from "./SharedModule/shared/material.module";
+import { MaterialModule } from "./shared/material.module";
 
 import { AppComponent } from "./app.component";
 import { CardComponent } from './card/card.component';
