dbee126810394382d412d140d19993c837bf713a
gf <gianfranco.festi@gmail.com>
2021-01-06 09:59:20 +0100

    problemi su https://codesandbox.io/ cond DragDropModule dovuto a versioni non compatibili

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -1,7 +1,7 @@
 import {NgModule} from '@angular/core';
 import {CommonModule } from '@angular/common';
 import {A11yModule} from '@angular/cdk/a11y';
-import {DragDropModule} from '@angular/cdk/drag-drop';
+//import {DragDropModule} from '@angular/cdk/drag-drop';
 import {PortalModule} from '@angular/cdk/portal';
 import {ScrollingModule} from '@angular/cdk/scrolling';
 import {CdkStepperModule} from '@angular/cdk/stepper';
